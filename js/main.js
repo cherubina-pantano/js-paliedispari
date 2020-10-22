b1c168f35dea836917902381d649594843dc5703
cherubina-pantano <cherubina.pantano@gmail.com>
2020-10-22 15:40:55 +0200

    Palindroma: eseguito con i metodi

--- js/main.js
+++ js/main.js
@@ -23,11 +23,20 @@ else {
 
 // Creazione funzione per invertire i caratteri della stringa
 function wordReverse(word) {
-  var inverti = '';
+  /* methods:
+  "split" -  prende i caratteri stringa e li trasforma in array
+  "reverse" - inverte i caratteri array
+  "join" - trasforma array di nuovo in stringa
+  */ 
 
-  for (var i = word.length - 1; i >= 0; i--) {
-    inverti += word[i];
-  }
+  var inverti = word.split('').reverse().join('');
+
+
+  // var inverti = '';
+  //
+  // for (var i = word.length - 1; i >= 0; i--) {
+  //   inverti += word[i];
+  // }
 
   return inverti;
 }
